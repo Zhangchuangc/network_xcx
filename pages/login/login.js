f482cd876d75cb429664f774c62f44199b4958a8
zhangchuangchuang <1374618878@qq.com>
2026-01-28 22:32:31 +0800

    完成我的界面搭建

--- pages/login/login.js
+++ pages/login/login.js
@@ -8,16 +8,81 @@ Page({
   },
 
 
-xieyiChange(e){
-  console.log(e),
-  this.setData({
-    checked:e.detail,
-  })
-},
+  xieyiChange(e) {
+    console.log(e),
+      this.setData({
+        checked: e.detail,
+      })
+  },
+
+  goxieyi(e) {
+    wx.navigateTo({
+      url: '/pages/about/index?key=' + e.currentTarget.dataset.key,
+    })
+  },
+
+  showModel2() {
+    wx.showModal({
+      title: '温馨提示',
+      content: '已经阅读并同意《用户协议》、《隐私协议》',
+      cancelText: '不同意',
+      confirmText: '同意',
+      success: res => {
+        if (res.confirm) {
+          this.setData({
+            checked: true
+          })
+        }
+      }
+    }
+    )
+  },
+  showModal(action) {
+    wx.showModal({
+      title: '温馨提示',
+      content: '已经阅读并同意《用户协议》、《隐私协议》',
+      cancelText: '不同意',
+      confirmText: '同意',
+      success: res => {
+        if (res.confirm) {
+          this.setData({
+            checked: true
+          })
+          if (action == 'loginOne') {
+            this.loginOne()
+          }
+        }
+      }
+    })
+  },
+
+  async loginOne(){
+    if(!this.data.checked){
+      this.showModal('longOne')
+      return
+    }
+    const res = await AUTH.login20241025()
+    if (res.code == 10000) {
+      // 用户不存在
+      wx.showModal({
+        content: '您还未注册，请使用《手机号安全登陆》方式登陆',
+        showCancel: false
+      })
+      return
+    }
+    if (res.code != 0) {
+      // 登录错误
+      return
+    }
+    wx.reLaunch({
+      url:'/pages/index/index',
+    })
+  },
 
 
 
 
+  
   /**
    * 生命周期函数--监听页面加载
    */
